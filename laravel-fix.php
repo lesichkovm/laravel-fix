8bffce3af3def3775914f23692e87ab143a73189
Milan Lesichkov <lesichkovm@gmail.com>
2015-11-13 07:45:00 +0000

    Create laravel-fix.php

---
+++ laravel-fix.php
@@ -0,0 +1,20 @@
+
+<?php
+
+$currentDir = __DIR__ . DIRECTORY_SEPARATOR;
+
+echo "START: Laravel fix\n";
+
+$cmd = 'chown -R www-data:www-data '.$currentDir.'storage/';
+echo cmd($cmd) . "\n";
+$cmd = 'chmod -R 775 '.$currentDir.'storage/';
+echo cmd($cmd) . "\n";
+
+echo "END: Laravel fix\n";
+
+/* Required functions */
+function cmd($cmd) {
+    echo "Executing command: $cmd\n";
+    $output = shell_exec($cmd);
+    return $output;
+}
