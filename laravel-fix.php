b3f93c4690b03538252306aab79ff77be908ecff
Milan Lesichkov <lesichkovm@gmail.com>
2017-07-09 04:59:57 +0100

    Update laravel-fix.php

--- laravel-fix.php
+++ laravel-fix.php
@@ -5,14 +5,19 @@ $currentDir = __DIR__ . DIRECTORY_SEPARATOR;
 
 echo "START: Laravel fix\n";
 
-$cmd = 'chown -R www-data:www-data '.$currentDir.'storage/';
+$cmd = 'chown -R www-data:www-data ' . $currentDir . 'storage/';
 echo cmd($cmd) . "\n";
-$cmd = 'chmod -R 775 '.$currentDir.'storage/';
+$cmd = 'chmod -R 775 ' . $currentDir . 'storage/';
+echo cmd($cmd) . "\n";
+$cmd = 'chown -R www-data:www-data ' . $currentDir . 'bootstrap/cache/';
+echo cmd($cmd) . "\n";
+$cmd = 'chmod -R 775 ' . $currentDir . 'bootstrap/cache/';
 echo cmd($cmd) . "\n";
 
 echo "END: Laravel fix\n";
 
 /* Required functions */
+
 function cmd($cmd) {
     echo "Executing command: $cmd\n";
     $output = shell_exec($cmd);
