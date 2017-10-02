28607aec29bd3da563a463b9750b84bb646971f2
Sinevia <info@sinevia.com>
2017-10-02 06:47:03 +0100

    Added fix for public folder

--- laravel-fix.php
+++ laravel-fix.php
@@ -5,13 +5,24 @@ $currentDir = __DIR__ . DIRECTORY_SEPARATOR;
 
 echo "START: Laravel fix\n";
 
+echo "1. Repair storage directory\n";
 $cmd = 'chown -R www-data:www-data ' . $currentDir . 'storage/';
 echo cmd($cmd) . "\n";
 $cmd = 'chmod -R 775 ' . $currentDir . 'storage/';
 echo cmd($cmd) . "\n";
+
+echo "2. Repair bootstrap/cache directory\n";
 $cmd = 'chown -R www-data:www-data ' . $currentDir . 'bootstrap/cache/';
 echo cmd($cmd) . "\n";
 $cmd = 'chmod -R 775 ' . $currentDir . 'bootstrap/cache/';
+echo cmd($cmd) . "\n";
+
+echo "3. Repair public directory\n";
+$cmd = 'chown -R www-data:www-data ' . $currentDir . 'public/';
+$output = cmd($cmd);
+echo $output . "\n";
+
+$cmd = 'chmod -R 770 ' . $currentDir . 'public/';
 echo cmd($cmd) . "\n";
 
 echo "END: Laravel fix\n";
