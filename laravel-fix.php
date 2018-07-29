d76f4148d3f2f39b777b1a05788e6319847a4988
Milan Lesichkov <lesichkovm@gmail.com>
2018-07-29 06:07:18 +0100

    Update laravel-fix.php

--- laravel-fix.php
+++ laravel-fix.php
@@ -5,23 +5,46 @@ $currentDir = __DIR__ . DIRECTORY_SEPARATOR;
 
 echo "START: Laravel fix\n";
 
-echo "1. Repair storage directory\n";
+echo "1. Removing vendor directory...\n";
+$cmd = 'rm -rf ' . $currentDir . 'vendor';
+echo cmd($cmd) . "\n";
+
+echo "2. Updating composer...\n";
+$cmd = 'composer update --prefer-dist -o';
+echo cmd($cmd) . "\n";
+
+echo "3. Changing overall owner to www-data...\n";
+$cmd = 'chown -R www-data:www-data ' . $currentDir . '*';
+$output = cmd($cmd);
+echo $output . "\n";
+
+echo "4. Changing overall mod to 770...\n";
+$cmd = 'chmod -R 770 ' . $currentDir . '*';
+$output = cmd($cmd);
+echo $output . "\n";
+
+echo "5. Repair all directories\n";
+$cmd = 'chown -R www-data:www-data ' . $currentDir . '*';
+echo cmd($cmd) . "\n";
+$cmd = 'chmod -R 775 ' . $currentDir . 'public/';
+echo cmd($cmd) . "\n";
+
+echo "6. Repair storage directory\n";
 $cmd = 'chown -R www-data:www-data ' . $currentDir . 'storage/';
 echo cmd($cmd) . "\n";
 $cmd = 'chmod -R 775 ' . $currentDir . 'storage/';
 echo cmd($cmd) . "\n";
 
-echo "2. Repair bootstrap/cache directory\n";
+echo "7. Repair bootstrap/cache directory\n";
 $cmd = 'chown -R www-data:www-data ' . $currentDir . 'bootstrap/cache/';
 echo cmd($cmd) . "\n";
 $cmd = 'chmod -R 775 ' . $currentDir . 'bootstrap/cache/';
 echo cmd($cmd) . "\n";
 
-echo "3. Repair public directory\n";
+echo "8. Repair public directory\n";
 $cmd = 'chown -R www-data:www-data ' . $currentDir . 'public/';
 $output = cmd($cmd);
 echo $output . "\n";
-
 $cmd = 'chmod -R 770 ' . $currentDir . 'public/';
 echo cmd($cmd) . "\n";
 
